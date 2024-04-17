ba2375979c2596dc013423f93913dab747716dfc
rohitdshinde <royshinde7744@gmail.com>
2024-04-17 21:39:41 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textFname = new System.Windows.Forms.TextBox();
+            this.textLname = new System.Windows.Forms.TextBox();
+            this.textAdd = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.textUser = new System.Windows.Forms.TextBox();
+            this.textPass = new System.Windows.Forms.TextBox();
+            this.comGender = new System.Windows.Forms.ComboBox();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Firstname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(492, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lastname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(113, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gender";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(492, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Phone";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(113, 307);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Username";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(492, 307);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Password";
+            // 
+            // textFname
+            // 
+            this.textFname.Location = new System.Drawing.Point(212, 69);
+            this.textFname.Name = "textFname";
+            this.textFname.Size = new System.Drawing.Size(100, 20);
+            this.textFname.TabIndex = 8;
+            // 
+            // textLname
+            // 
+            this.textLname.Location = new System.Drawing.Point(592, 69);
+            this.textLname.Name = "textLname";
+            this.textLname.Size = new System.Drawing.Size(100, 20);
+            this.textLname.TabIndex = 9;
+            // 
+            // textAdd
+            // 
+            this.textAdd.Location = new System.Drawing.Point(231, 135);
+            this.textAdd.Name = "textAdd";
+            this.textAdd.Size = new System.Drawing.Size(100, 20);
+            this.textAdd.TabIndex = 10;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(231, 210);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(100, 20);
+            this.textEmail.TabIndex = 12;
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(565, 203);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(100, 20);
+            this.textPhone.TabIndex = 13;
+            // 
+            // textUser
+            // 
+            this.textUser.Location = new System.Drawing.Point(231, 307);
+            this.textUser.Name = "textUser";
+            this.textUser.Size = new System.Drawing.Size(100, 20);
+            this.textUser.TabIndex = 14;
+            // 
+            // textPass
+            // 
+            this.textPass.Location = new System.Drawing.Point(565, 300);
+            this.textPass.Name = "textPass";
+            this.textPass.PasswordChar = '*';
+            this.textPass.Size = new System.Drawing.Size(100, 20);
+            this.textPass.TabIndex = 15;
+            // 
+            // comGender
+            // 
+            this.comGender.FormattingEnabled = true;
+            this.comGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Other"});
+            this.comGender.Location = new System.Drawing.Point(571, 132);
+            this.comGender.Name = "comGender";
+            this.comGender.Size = new System.Drawing.Size(121, 21);
+            this.comGender.TabIndex = 16;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(571, 372);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 17;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.comGender);
+            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textAdd);
+            this.Controls.Add(this.textLname);
+            this.Controls.Add(this.textFname);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textFname;
+        private System.Windows.Forms.TextBox textLname;
+        private System.Windows.Forms.TextBox textAdd;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.ComboBox comGender;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            SqlConnection con=new SqlConnection("Data Source=DESKTOP-FF8E3E6\\SQLEXPRESS;Initial Catalog=userregcs;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand(@"INSERT INTO [dbo].[register]
+           ([firstname]
+           ,[lastname]
+           ,[address]
+           ,[gender]
+           ,[email]
+           ,[phone]
+           ,[username]
+           ,[password])
+     VALUES
+           ('" + textFname.Text + "','" + textLname.Text + "','" + textAdd.Text + "','" + comGender.SelectedItem.ToString() + "','" + textEmail.Text + "','" + textPhone.Text + "','" + textUser.Text + "','" + textPass.Text + "')",con);
+             con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("REgister successfully");
+        }
+    }
+}
